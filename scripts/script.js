c99f82e60cf01c5c7739ad1997376413fe216a14
Gabriel Palhares <gabriel.palhares@autoglass.com.br>
2021-12-10 16:51:20 -0300

    implementado primeira versao da tela mesas

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function jogar()
+  {
+    var resp = document.getElementById('resp');
+    resp.innerHTML = Math.floor( 1 + 20*Math.random() );
+  }
+
+  function novoAtributo(){
+      var divAtributo = document.getElementById('novosAtributos');
+      
+  }
